6e3c1ee653e0bbe7bfa6bfcb377343b6c66a9a7d
sean-m-spencer <seanspencer95@gmail.com>
2021-08-01 16:38:22 -0400

    Added some routes and the start of the templates.

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 from src import create_app
+
+#prevents the user from invoking a script on import if they don't mean too.
+if __name__ == "__main__":   
+    app = create_app()
+    #Anytime you make a change to codebase it'll restart server while set to true
+    app.run(debug = True)
+
--- src/__init__.py
+++ src/__init__.py
@@ -9,4 +9,11 @@ def create_app():
     #BEFORE DEPLOYMENT CHANGE KEY 
     app.config['SECRET_KEY'] = "helloworld"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix = "/")
+    app.register_blueprint(auth, url_prefix = "/")
+
+
     return app
